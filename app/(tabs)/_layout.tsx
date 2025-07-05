eaef2c84be3e5f6d99202b4371444f4c289e98ae
nrohitydv <nrohityadavv@gmail.com>
2025-07-05 20:04:08 +0545

    Add authentication layout and screens for sign-in and sign-up

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Redirect, Slot } from "expo-router";
+
+const _Layout = () => {
+  const isAuthenticated = false;
+  if (!isAuthenticated) return <Redirect href="/sign-in" />;
+  return <Slot />;
+};
+
+export default _Layout;
