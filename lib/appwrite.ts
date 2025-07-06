15e4bde828e8d67c75df2c55ab03f2eaa2c1a2c1
nrohitydv <nrohityadavv@gmail.com>
2025-07-06 14:11:41 +0545

    appwrite config done

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  platform: "com.rohit.fastfood",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+};
