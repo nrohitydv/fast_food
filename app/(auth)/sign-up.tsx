33553625a36862b4fdc5c64feade9f56a60f3b1c
nrohitydv <nrohityadavv@gmail.com>
2025-07-06 23:58:27 +0545

    Refactor authentication screens and layout; add custom input and button components

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,68 @@
-import { router } from "expo-router";
-import React from "react";
-import { Button, Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignUp = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const submit = async () => {
+    if (!form.email || !form.password || !form.name)
+      return Alert.alert(
+        "Error",
+        "Please enter valid name, email and password"
+      );
+    setIsSubmitting(true);
+    try {
+      //call appwrite sign in function
+      Alert.alert("Success", "Signed up successfully");
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message || "Something went wrong");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
-    <View>
-      <Text>SignUp</Text>
-      <Button title="Sign In" onPress={() => router.push("/sign-in")} />
+    <View className="gap-10 bg-white p-5 mt-t rounded-lg">
+      <CustomInput
+        placeholder="Enter your full name"
+        value={form.name}
+        onChangeText={(text) => {
+          setForm((prev) => ({ ...prev, name: text }));
+        }}
+        label="Full Name"
+      />
+      <CustomInput
+        placeholder="Enter your email"
+        value={form.email}
+        onChangeText={(text) => {
+          setForm((prev) => ({ ...prev, email: text }));
+        }}
+        label="Email"
+        keyboardType="email-address"
+      />
+      <CustomInput
+        placeholder="Enter your password"
+        value={form.password}
+        onChangeText={(text) => {
+          setForm((prev) => ({ ...prev, password: text }));
+        }}
+        label="Password"
+        secureTextEntry={true}
+      />
+      <CustomButton title="Sign Up" isLoading={isSubmitting} onPress={submit} />
+      <View className="flex justify-center mt-5 flex-row gap-2">
+        <Text className="base-regular">Already have an account?</Text>
+        <Link href="/sign-in" className="base-bold text-primary">
+          Sign In
+        </Link>
+      </View>
     </View>
   );
 };
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   platform: "com.rohit.fastfood",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID,
 };
