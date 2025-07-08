9a50feb41854ac20986a9dbb4c23776140665835
nrohitydv <nrohityadavv@gmail.com>
2025-07-09 01:46:01 +0545

    Implement search functionality with menu and category retrieval; add MenuCard component for displaying menu items

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { CreateUserParams, SignInParams } from "@/type";
+import { CreateUserParams, GetMenuParams, SignInParams } from "@/type";
 import {
   Account,
   Avatars,
@@ -86,5 +86,35 @@ export const getCurrentUser = async () => {
   } catch (e) {
     console.log(e);
     throw new Error(e as string);
+  }
+};
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+  try {
+    const queries: string[] = [];
+    if (category) queries.push(Query.equal("categories", category));
+    if (query) queries.push(Query.search("name", query));
+
+    const menu = await databases.listDocuments(
+      appwriteConfig.databaseId!,
+      appwriteConfig.menuCollectionId!,
+      queries
+    );
+
+    return menu.documents;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const categories = await databases.listDocuments(
+      appwriteConfig.databaseId!,
+      appwriteConfig.categoriesCollectionId!
+    );
+    return categories.documents;
+  } catch (error) {
+    throw new Error(error as string);
   }
 };
