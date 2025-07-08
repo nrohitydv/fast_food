5bdb847e167bc22e4bb101e25b829010b59c86c8
nrohitydv <nrohityadavv@gmail.com>
2025-07-08 17:29:59 +0545

    created differents collections

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -13,7 +13,15 @@ export const appwriteConfig = {
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
   userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID,
+  categoriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_CATEGORIES_COLLECTION_ID,
+  menuCollectionId: process.env.EXPO_PUBLIC_APPWRITE_MENU_COLLECTION_ID,
+  customizationsCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_CUSTOMIZATIONS_COLLECTION_ID,
+  menuCustomizationsCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_MENU_CUSTOMIZATIONS_COLLECTION_ID,
 };
 
 export const client = new Client();
